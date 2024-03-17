c6389f5b6fae9aa11fc9957118b81d467fe90337
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2024-03-17 16:52:17 +0200

    changes to layout

--- TODO/js/index.js
+++ TODO/js/index.js
@@ -4,6 +4,57 @@ const list = document.querySelector("ul");
 const input = document.querySelector("input");
 
 input.disabled = true;
+
+
+const renderTask = (task) => {
+    const li = document.createElement("li");
+    li.setAttribute("class","list-group-item")
+    li.innerHTML = task;
+    list.append(li);
+}
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const task = input.value.trim();
+        if (task !== "") {
+            savetask(task).then((json) => {
+            renderTask(task);
+            input.value = "";
+        })
+     }
+   }
+})
+
+const getTasks = async () => {
+    try {
+        const response = await fetch(BACKEND_ROOT_URL);
+        const json = await response.json();
+        json.forEach (task => {
+            renderTask(task.description);
+        });
+        input.disabled = false;
+    }   catch(error){
+        alert("Error  retrieving tasks " + error.message);   
+    }
+} 
+const savetask = async (task) => {
+        try {
+            const json = JSON.stringify({description: task});
+            const response = await fetch(BACKEND_ROOT_URL + "/new", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: json
+            });
+            return response.json();
+        } catch (error) {
+            alert("Error saving task " + error.message);
+          } 
+        }
+
+ getTasks();
+
 input.addEventListener("keypress",(event) => {
     if (event.key === "Enter"){
         event.preventDefault();
@@ -18,54 +69,4 @@ input.addEventListener("keypress",(event) => {
     }
 });
 
-
-
-const renderTask = (task) => {
-    const li = document.createElement("li");
-    li.setAttribute("class","list-group-item")
-    li.innerHTML = task;
-    list.append(li);
-}
-
-const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL);
-        const json = await response.json();
-        json.forEach (task => {
-            renderTask(task.description);
-        });
-        input.disabled = false;
-    }   catch(error){
-        alert("Error  retrieving tasks " + error.message);   
-    }
-}
-    const savetask = async (task) => {
-        try {
-            const json = JSON.stringify({description: task});
-            const response = await fetch(BACKEND_ROOT_URL + "/new", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: json
-            });
-            return response.json();
-        } catch (error) {
-            alert("Error saving task " + error.message);
-          } 
-        }
-
-input.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        const task = input.value.trim();
-        if (task !== "") {
-            savetask(task).then((json) => {
-            renderTask(task);
-            input.value = "";
-        })
-     }
-   }
-})
-
-getTasks();
+   
