2ffacc2123d793cc1bb44cc8229018ded189d1e5
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2024-03-18 19:00:09 +0200

    commitig part 4 and 3 of todo

--- TODO/js/index.js
+++ TODO/js/index.js
@@ -1,85 +1,8 @@
-/*
-const BACKEND_ROOT_URL = "http://localhost:3001";
-
-const list = document.querySelector("ul");
-const input = document.querySelector("input");
-
-input.disabled = true;
-
-
-const renderTask = (task) => {
-    const li = document.createElement("li");
-    li.setAttribute("class","list-group-item")
-    li.innerHTML = task;
-    list.append(li);
-}
-
-
-const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL);
-        const json = await response.json();
-        //console.log(json); // Log the response to see its structure
-        json.forEach( task => {
-            renderTask(task.description);
-        }) // Commented out for debugging
-        input.disabled = false;
-    } catch(error) {
-        console.log(error);
-        alert("Error retrieving tasks " + error.message);   
-    }
-}
-
-const savetask = async (task) => {
-        try {
-            const json = JSON.stringify({description: task});
-            const response = await fetch(BACKEND_ROOT_URL + "/new", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: json
-            });
-            return response.json();
-        } catch (error) {
-            console.log (error)
-            alert("Error saving task " + error.message);
-          } 
-        }
-
-
-input.addEventListener("keypress",(event) => {
-    if (event.key === "Enter"){
-        event.preventDefault();
-        const task=input.value.trim();
-    if (task !== "") {
-        const li = document.createElement("li");
-        li.setAttribute("class","list-group-item")
-        li.innerHTML = task;
-        list.append(li);
-        input.value = "";
-        }
-    }
-});
-
-input.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        const task = input.value.trim();
-        if (task !== "") {
-            savetask(task).then((json) => {
-            renderTask(task);
-            input.value = "";
-        })
-     }
-   }
-})
-
-getTasks();
-
-   */
 
 const BACKEND_ROOT_URL = 'http://localhost:3001';
+import { Todos } from './class/Todos.js';
+
+const todos = new Todos(BACKEND_ROOT_URL);
 
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
@@ -90,22 +13,20 @@ input.disabled = true;
 const renderTask = (task) => {
     const li = document.createElement('li');
     li.setAttribute('class', 'list-group-item');
-    li.innerHTML = task;
+    li.innerHTML = task.getText();
     list.appendChild(li);
 }
 
-const getTasks = async () => {
-    try {
-        const response = await fetch(BACKEND_ROOT_URL);
-        const json = await response.json();
-        json.forEach(task => {
-            renderTask(task.description);
-        });
-        input.disabled = false;
-    } catch (error) {
-        console.error('Error:', error);
-        alert('Error fetching tasks' + error);
-    }
+const getTasks = () => {
+   todos.getTasks().then(tasks => {
+         tasks.forEach(task => {
+              renderTask(task);
+         })
+         input.disabled = false;
+         
+         }) .catch(error => {
+            alert(error)
+      })  
 }
 
 const saveTask = async (task) => {
@@ -128,20 +49,17 @@ const saveTask = async (task) => {
 getTasks();
 
 
-input.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
-        e.preventDefault();
+input.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+        event.preventDefault();
         const task = input.value.trim();
         if (task !=='') {
-            const li = document.createElement('li');
-            li.setAttribute('class', 'list-group-item');
-            //li.innerHTML = task;
-           // list.appendChild(li);
-            input.value = '';
-            saveTask(task).then((json) => {
+            todos.addTask(task).then((task) => {
                 renderTask(task);
                 input.value = '';
-            });
-        } 
+            })
+           input.focus();
+        }
     }
-});
+ })
+
