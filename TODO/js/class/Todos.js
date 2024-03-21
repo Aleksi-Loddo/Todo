3d3a21e04ac64e69a60f633d93e8b3409d1f482a
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2024-03-21 12:22:14 +0200

    part 7 commit

--- TODO/js/class/Todos.js
+++ TODO/js/class/Todos.js
@@ -1,87 +1,4 @@
 
-/* import { Task } from './Task.js';
-
-class Todos {
-    #tasks = [];
-    #backend_url = '';
-
-    constructor(url) {
-        this.#backend_url = url;
-    }
-
-    getTasks = () => {
-        return new Promise(async(resolve, reject) => {
-            fetch(this.#backend_url)
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json)
-                this.#readJson(json)
-                resolve(this.#tasks);
-            }, error => {
-                reject(error);
-            })
-        })
-    }
-
-    #readJson = (tasksAsJson) => {
-        if (!Array.isArray(tasksAsJson)) {
-            console.error("Tasks data is not in the expected format.");
-            return;
-        }
-        console.log(tasksAsJson) // troubleshooting  message
-        tasksAsJson.forEach(node => {
-            const task = new Task(node.id, node.description);
-            this.#tasks.push(task);
-        });
-    }
-
-    #addToArray = (id, text) => {
-        const task = new Task(id, text);
-        this.#tasks.push(task);
-        return task;
-    }
-    
-    removeTask = (id) => {  
-        return new  Promise(async(resolve, reject) => {
-            fetch(this.#backend_url + "/delete/" + id, {
-                method: 'DELETE'
-            })
-            .then((response) => response.json())
-            .then((json) => {
-                this.#removeFromArray(id)
-                resolve(json.id)
-            }, error => {
-                reject(error);
-            })
-        })
-    }
-    
-    #removeFromArray = (id) => {
-        const arrayWithoutRemoved = this.#tasks.filter(task => task.id !== id);
-        this.#tasks =  arrayWithoutRemoved
-    }
-    addTask = (text) => {
-        return new Promise(async(resolve, reject) => {
-            const json = JSON.stringify({description: text});
-            fetch(this.#backend_url + "/new", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: json
-            })
-            .then((response) => response.json())
-            .then((json) => {
-                resolve(this.#addToArray(json.id, text));
-
-            }, error => {
-                reject(error);
-        })
-    })
-  }
-}
-
-export { Todos }  */
 
 import { Task } from './Task.js';
 
@@ -91,8 +8,9 @@ class Todos {
 
     constructor(url) {
         this.#backend_url = url;
+        
     }
-
+     
     getTasks = () => {
         return new Promise(async(resolve, reject) => {
             fetch(this.#backend_url)
@@ -106,10 +24,12 @@ class Todos {
         })
     }
 
-    #readJson = (tasksAsJson) => {
+    #readJson = (tasksAsJson) => {  
+        console.log(tasksAsJson);
         tasksAsJson.forEach(node => {
             const task = new Task(node.id, node.description);
             this.#tasks.push(task);
+           
         });
     }
 
