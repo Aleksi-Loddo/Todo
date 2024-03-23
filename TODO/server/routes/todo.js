f2d888a455a9c99ea3251a6bec522a53851c3851
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2024-03-23 16:09:56 +0200

    commit change to database correction on route file

--- TODO/server/routes/todo.js
+++ TODO/server/routes/todo.js
@@ -18,7 +18,7 @@ todoRouter.get('/', async (req, res) => {
 
 todoRouter.post('/new', async (req, res) => {
     try {
-        const result = await query('INSERT INTO tasks (description) VALUES ($1) RETURNING *', 
+        const result = await query('INSERT INTO task (description) VALUES ($1) RETURNING *', 
         [req.body.description]);
         res.status(200).json({id: result.rows[0].id});
         } catch (error)  {
@@ -31,7 +31,7 @@ todoRouter.post('/new', async (req, res) => {
 todoRouter.delete('/delete/:id', async (req, res) => {  
     const id = Number(req.params.id);
     try {
-        const result = await query('DELETE FROM tasks WHERE id = $1',
+        const result = await query('DELETE FROM task WHERE id = $1',
          [id]);
         res.status(200).json({id: id});
     } catch (error) {
